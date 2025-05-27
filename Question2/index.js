213ee765ee11b0300b11e7fa376fe9b0ce2f12bd
fana <fanabezabih@gmail.com>
2025-05-27 11:25:07 +0300

    Did an answer for question2

--- Question2/index.js
+++ Question2/index.js
@@ -1,44 +1,39 @@
-class Appointment {
-  constructor(patientName, startTime, endTime, priority, specialty) {
-    this.patientName = patientName;
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.priority = priority; 
-    this.specialty = specialty;
-  }
-}
-class Scheduler {
-  constructor() {
-    this.doctorSchedules = {}; 
-    this.emergencyQueue = []; 
-  }
-  addDoctor(name) {
-    this.doctorSchedules[name] = [];
-  }
-  scheduleAppointment(doctorName, appointment) {
-    if (!this.doctorSchedules[doctorName]) return "Doctor not found";
-    this.doctorSchedules[doctorName].push(appointment);
-   
-    this.doctorSchedules[doctorName].sort((a, b) => a.startTime - b.startTime);
-  }
-  addEmergency(appointment) {
-    this.emergencyQueue.push(appointment);
-    this.emergencyQueue.sort((a, b) => a.priority - b.priority); 
-  }
-}
+    function checkAvailability(doctorAvailability, requestedDate) {
+    
+        return doctorAvailability.includes(requestedDate); 
+    }
+        
+    function findMatchingDoctors(doctors, requestedDate, isEmergency) {
+        const availableDoctors = [];
 
-let scheduler = new Scheduler();
-scheduler.addDoctor("Dr.Fana");
-let appt = new Appointment("Hellen", new Date("2025-06-01T10:00"), new Date("2025-06-01T10:30"), 3, "Dermatology");
-scheduler.scheduleAppointment("Dr.Fana", appt);
-let emergency = new Appointment("EmergencyPatient", new Date(), new Date(Date.now() + 30 * 60000), 1, "Cardiology");
-scheduler.addEmergency(emergency);
+        if (isEmergency) {
+            
+            for (const doctor of doctors) {
+                if (doctor.availability.length > 0) { 
+                  availableDoctors.push(doctor);
 
+                }
+            }
+        } else {
+            
+            for (const doctor of doctors) {
+                if (checkAvailability(doctor.availability, requestedDate)) {
+                   availableDoctors.push(doctor);
+                }
+            }
+        }
+        return availableDoctors;
+    }
+        const doctors = [
+        { name: "Dr. Fana",  specialty: "Cardiology", availability: ["05/27/2025", "05/28/2025"] },
+        { name: "Dr. Daniel", specialty: "Neurology", availability: ["05/29/2025", "05/30/2025"] },
+    ];
 
+    const patient1 = { name: "Hellen", requestedDate: "05/27/2025", isEmergency: false };
+    const patient2 = { name: "Jennifer", requestedDate: "05/27/2025", isEmergency: true };
 
+    const matchingDoctorsForPatient1 = findMatchingDoctors(doctors, patient1.requestedDate, patient1.isEmergency);
+    const matchingDoctorsForPatient2 = findMatchingDoctors(doctors, patient2.requestedDate, patient2.isEmergency);
 
-
-
-
-
-
+    console.log(matchingDoctorsForPatient1); 
+    console.log(matchingDoctorsForPatient2); 
